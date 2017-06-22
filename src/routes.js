a8eb3d789da9459c707273936687b02f81578f59
alpsln <alpsln78@yahoo.com>
2017-06-22 19:41:11 +0800

    切换分支

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
-    App,
+    App, 
     Chat,
     Home,
     Widgets,
